ab2b8c63f0035ec7865b9068123ecf023a5b0d60
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-13 03:00:09 +0600

    error anime added in error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,7 @@
 import Button from '../components/Shared/Button/Button'
 import { useNavigate } from 'react-router-dom'
+import Lottie from "lottie-react";
+import error from "../../public/error.json";
 
 const ErrorPage = () => {
   const navigate = useNavigate()
@@ -9,7 +11,7 @@ const ErrorPage = () => {
       <div className='container flex items-center min-h-screen px-6 py-12 mx-auto'>
         <div className='flex flex-col items-center max-w-sm mx-auto text-center'>
           <p className='p-3 text-sm font-medium text-orange-500 rounded-full bg-blue-50 '>
-            <svg
+            {/* <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
               viewBox='0 0 24 24'
@@ -22,7 +24,8 @@ const ErrorPage = () => {
                 strokeLinejoin='round'
                 d='M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z'
               />
-            </svg>
+            </svg> */}
+              <Lottie animationData={error} loop={true} />
           </p>
           <h1 className='mt-3 text-2xl font-semibold text-gray-800  md:text-3xl'>
             There was some error!!!
