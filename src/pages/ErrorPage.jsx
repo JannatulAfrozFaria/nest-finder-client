a0a39a24e2ebc9f59527630bd06bc28d62fa177c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-13 02:29:15 +0600

    error page created

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -25,9 +25,9 @@ const ErrorPage = () => {
             </svg>
           </p>
           <h1 className='mt-3 text-2xl font-semibold text-gray-800  md:text-3xl'>
-            Something Went Wrong!
+            Error!!!
           </h1>
-          <p className='mt-4 text-gray-500 '>Here are some helpful links:</p>
+          <p className='mt-4 text-gray-500 '>For help, Navigate to:</p>
 
           <div className='flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto'>
             <button
