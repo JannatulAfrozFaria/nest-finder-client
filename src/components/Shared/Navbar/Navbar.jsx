a47daeb0bd4a84bbe5c251d5fbd5d689ceca39c3
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-13 02:25:23 +0600

    navbar modified

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
-      <div className='py-1 border-b-[1px]'>
+      <div className='py-2 border-b-[1px]'>
         <Container>
           <div className='flex flex-row  items-center justify-between gap-3 md:gap-0'>
             {/* Logo */}
