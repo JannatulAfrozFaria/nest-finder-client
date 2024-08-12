bdfdd4630748b944b1648c960da15a83480762d3
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-08-13 03:05:40 +0600

    error page modified

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -10,21 +10,7 @@ const ErrorPage = () => {
     <section className='bg-white '>
       <div className='container flex items-center min-h-screen px-6 py-12 mx-auto'>
         <div className='flex flex-col items-center max-w-sm mx-auto text-center'>
-          <p className='p-3 text-sm font-medium text-orange-500 rounded-full bg-blue-50 '>
-            {/* <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth='2'
-              stroke='currentColor'
-              className='w-6 h-6'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z'
-              />
-            </svg> */}
+          <p className='p-3 w-1/2 md:w-2/3 text-sm font-medium text-orange-500 rounded-full bg-blue-50 '>
               <Lottie animationData={error} loop={true} />
           </p>
           <h1 className='mt-3 text-2xl font-semibold text-gray-800  md:text-3xl'>
